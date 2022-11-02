abd5dd37287ae0505a2944f297322d4e15a3fa14
fularani <ranivhansure@gmail.com>
2022-11-02 11:06:03 +0530

    routing in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
 import './App.css';
+import Page1 from './Components/Page1';
+import Page2 from './Components/Page2';
+import Page3 from './Components/Page3';
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+        <Route exact path="/" element={<h1>Home Page</h1>} />
+        <Route exact path="/page1" element={<Page1 />} />
+        <Route exact path="/page2" element={<Page2 />} />
+        <Route exact path="/page3" element={<Page3 />} />
+        </Routes>
+        <div className="list">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="page1">Page 1</Link></li>
+            <li><Link to="page2">Page 2</Link></li>
+            <li><Link to="page3">Page 3</Link></li>
+          </ul>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
