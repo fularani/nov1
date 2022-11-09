36f4ef9767945818f35576fb6e47f62412b70b2d
fularani <ranivhansure@gmail.com>
2022-11-09 12:17:48 +0530

    SearchBar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router, Route ,Link, Routes} from "react-router-dom";
-import './App.css';
-import Page1 from './Components/Page1';
-import Page2 from './Components/Page2';
-import Page3 from './Components/Page3';
+import * as React from 'react'
+import axios from "axios";
+import Lists from './Components/Lists';
+import SearchBar from './Components/SearchBar';
+import "./App.css";
 
 const App = () => {
 
+  const [users, setUsers] = React.useState([])
+  const [store, setStore] = React.useState([])
+  const [loading, setLoading] = React.useState(false)
+
+  const getUsers=()=>{
+        axios
+          .get("https://randomuser.me/api/?results=10&inc=name,registered&nat=fr")
+          .then(res => {
+            const newdata=res.data.results.map((result)=>({
+              name:`${result.name.first} ${result.name.last}`,
+              id:result.registered
+            }))
+            setLoading(true)
+            setUsers(newdata)
+            setStore(newdata)
+          })
+          .catch(err => console.error(err));
+  }
+  
+  React.useEffect(() => {
+    getUsers()
+  }, [])
+
+  const filterNames=(event)=>{
+      const filteredData=store.filter((item)=>(
+        item.name.toLowerCase().includes(event.target.value.toLowerCase())
+      ))
+      setUsers(filteredData)
+  }
+  
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-        <Route exact path="/" element={<h1>Home Page</h1>} />
-        <Route exact path="/page1" element={<Page1 />} />
-        <Route exact path="/page2" element={<Page2 />} />
-        <Route exact path="/page3" element={<Page3 />} />
-        </Routes>
-        <div className="list">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="page1">Page 1</Link></li>
-            <li><Link to="page2">Page 2</Link></li>
-            <li><Link to="page3">Page 3</Link></li>
-          </ul>
-        </div>
-      </Router>
-    </div>
+    <div>
+    {loading?(
+      <div className='card'>
+      <div className='header'>
+      NAME LIST
+      </div>      
+      <SearchBar searchFunction={filterNames}/>
+      <Lists usernames={users}/>
+      </div>
+    ):<div className='loader'></div>}
+    </div>    
   )
 }
 
